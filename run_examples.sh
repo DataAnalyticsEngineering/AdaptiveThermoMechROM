be7a289379aae4665484a2f17cfece07bed40a4d
Shadi <shadisharba@gmail.com>
2023-02-20 21:06:42 +0100

    remove extra file

--- run_examples.sh
+++
@@ -1,8 +0,0 @@
-mkdir output
-/usr/bin/time -v -o output/eg2.time python -u eg2_compare_approximations.py > output/eg2.out
-# /usr/bin/time -v -o output/eg3.time python -u eg3_hierarchical_sampling.py > output/eg3.out
-
-# https://github.com/remykarem/python2jupyter
-# pip install p2j
-# p2j eg5_FFANN.py
-# reverse: p2j p2j -r eg5_FFANN.ipynb
